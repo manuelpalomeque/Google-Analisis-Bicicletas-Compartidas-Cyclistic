9146e94b7269dddc9e54e9e69e1c5802aeb94702
Jonathan Manuel Palomeque <87273052+manuelpalomeque@users.noreply.github.com>
2024-04-01 13:13:05 -0300

    Inicio de script -
    Etapa inicial

---
+++ Proyecto final script.R
@@ -0,0 +1,14 @@
+# Autor: Palomeque Jonathan
+
+# 2- Preparar Datos:
+
+# A) Instalar librerias:
+ install.packages("tidyverse")
+
+ # B) Cargar librerias:
+
+# C) Importar Csv:
+
+# D) Ordenar por fecha
+
+
