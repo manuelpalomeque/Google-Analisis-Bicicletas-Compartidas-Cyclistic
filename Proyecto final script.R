28d5e3a2e112d9d8d9975614602ee628e1c39c40
manuelpalomeque <pjonathanmanuel@hotmail.com>
2024-04-28 17:38:38 -0300

    Cargar tablas

--- Proyecto final script.R
+++ Proyecto final script.R
@@ -8,7 +8,22 @@ install.packages("tidyverse")
 # B) Cargar librerias:
 library(tidyverse)
 
-# C) Importar Csv:
+# C) Importar Archivos Csv:
+febrero_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202302-divvy-tripdata.csv")
+marzo_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202303-divvy-tripdata.csv")
+abril_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202304-divvy-tripdata.csv")
+mayo_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202305-divvy-tripdata.csv")
+junio_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202306-divvy-tripdata.csv")
+julio_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202307-divvy-tripdata.csv")
+agosto_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202308-divvy-tripdata.csv")
+septiembre_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202309-divvy-tripdata.csv")
+octubre_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202310-divvy-tripdata.csv")
+noviembre_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202311-divvy-tripdata.csv")
+diciembre_2023 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202312-divvy-tripdata.csv")
+enero_2024 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202401-divvy-tripdata.csv")
+febrero_2024 <- read_csv("D:/GitHub/An-lisis-Bicicletas-Compartidas-Cyclistic/DataSet/202402-divvy-tripdata.csv")
+
+View(febrero_2023)
 
 # D) Ordenar por fecha
 
